8772c235b9076071cfbdc5a1a47e28a46bb3d967
Ivan Yarochkin <Strelok.Vano97@gmail.com>
2016-11-05 09:19:16 +0300

    Test clicking button

--- app/src/main/java/by/litelife/litelife_timetable/activity/MainActivity.java
+++ app/src/main/java/by/litelife/litelife_timetable/activity/MainActivity.java
@@ -3,16 +3,33 @@ package by.litelife.litelife_timetable.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import by.litelife.litelife_timetable.R;
 
 /**
  * Created by Mikhail Kazyuchyts on 11/4/2016.
  */
 public class MainActivity extends Activity {
+    private Button buttonClick;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
+
+        buttonClick = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        buttonClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Кликнул");
+            }
+        });
+
     }
 }
